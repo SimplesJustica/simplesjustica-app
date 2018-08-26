54dc1a77a6beac3fbae0a0a96d8be84870437494
Guilherme Camargo <guilherme.camargo@vidibr.com>
2018-08-26 20:56:07 -0300

    ListEmpresaAdapter

---
+++ app/src/main/java/com/example/guisfits/simplesjustica/ListEmpresaAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.guisfits.simplesjustica
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class ListExampleAdapter(context: Context, list: ArrayList<EmpresaModel>) : BaseAdapter() {
+    var mInflator: LayoutInflater
+    var empresaList: ArrayList<EmpresaModel>
+
+    init {
+        this.empresaList = list
+        this.mInflator = LayoutInflater.from(context)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+        var view: View?
+
+        if(convertView == null){
+            view = this.mInflator.inflate(R.layout.list_row, parent, false)
+        }
+        else {
+            view = convertView
+        }
+
+        var txtNome: TextView
+        var txtDescricao: TextView
+        var txtDemandas: TextView
+
+        txtNome = view?.findViewById(R.id.nome) as TextView
+        txtDescricao = view?.findViewById(R.id.descricao) as TextView
+        txtDemandas = view?.findViewById(R.id.demandas) as TextView
+
+        var model: EmpresaModel
+        model = getItem(position) as EmpresaModel
+
+        txtNome.setText(model.nome)
+        txtDescricao.setText(model.descricao)
+        txtDemandas.setText(model.demandas)
+
+        return view
+    }
+
+    override fun getCount(): Int {
+        return empresaList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return empresaList.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+}
--- app/src/main/java/com/example/guisfits/simplesjustica/MainActivity.kt
+++ app/src/main/java/com/example/guisfits/simplesjustica/MainActivity.kt
@@ -1,59 +1,24 @@
 package com.example.guisfits.simplesjustica
 
-import android.content.Context
-import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.RelativeLayout
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lv = findViewById(R.id.list) as ListView
-        lv.adapter = ListExampleAdapter(this)
-    }
 
+        var empresaList: ArrayList<EmpresaModel>
+        empresaList = ArrayList()
+        empresaList.add(EmpresaModel("Net", "abc", 3))
+        empresaList.add(EmpresaModel("Claro", "acsdfs", 5))
+        empresaList.add(EmpresaModel("Vivo", "a3454", 7))
 
-    private class ListExampleAdapter(context: Context) : BaseAdapter() {
+        var adapter: ListExampleAdapter
 
-        val empresaNet = EmpresaModel("Net", "jasdhsdjkfhjk sd", 3)
-        internal var empresaList = arrayOf<EmpresaModel>(empresaNet, EmpresaModel("Claro", "asdfsdfds", 109))
-
-        private val mInflator: LayoutInflater
-
-        init {
-            this.mInflator = LayoutInflater.from(context)
-        }
-
-        override fun getCount(): Int {
-            return empresaList.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return empresaList[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-            val view: View?
-            if (convertView == null) {
-                view = this.mInflator.inflate(R.layout.list_row, parent, false)
-            } else {
-                view = convertView
-            }
-            // TODO Setar os elementos da view pros elementos da lista
-            return view
-        }
+        adapter = ListExampleAdapter(context = this, list = empresaList)
+        empresaListView.adapter = adapter
     }
 }
